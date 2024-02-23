120f432fad5fcff053723e9cd1790683dec33551
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-02-23 19:54:36 +0500

    Add files via upload
    Bano Qabil is the Largest IT scholarship Program for the Youth of Karachi,thank you very much Alkhidmat that you started this kind of advanced IT courses and IT, training series.

---
+++ myjavaScript.js
@@ -0,0 +1,7 @@
+/*let myVar = setInterval(myTimer ,1000);
+    function myTimer() {
+      const d = new Date();
+      document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+    }
+*/
+// time 2
